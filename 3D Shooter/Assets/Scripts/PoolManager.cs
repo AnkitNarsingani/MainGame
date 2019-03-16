c481b9cd8d79a80f335039f4154d823e4667633f
RonakFabain <ronakmanfabiaan@gmail.com>
2019-03-16 20:49:32 +0530

    objPool

---
+++ 3D Shooter/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+
+    static PoolManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+
+    public class Objects
+    {
+        public GameObject bullet;
+        public int max_Size;
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
