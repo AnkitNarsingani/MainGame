714e084f8f19da5a4390a8a098d4256155a049b9
RonakFabain <ronakmanfabiaan@gmail.com>
2019-04-17 19:47:44 +0530

    Kill me Plz

--- 3D Shooter/Assets/Scripts/GunProjectille.cs
+++ 3D Shooter/Assets/Scripts/GunProjectille.cs
@@ -15,8 +15,10 @@ public class GunProjectille : Gun
     Touch touch;
     private Vector3 delta = Vector3.zero;
     Vector3 gettouch;
+    Vector3 currentScreenPos, screenPos, currentPos;
     Vector3 offset;
     Vector3 loc;
+    float deltaX, deltaY;
     bool stayed;
     void Start()
     {
@@ -43,50 +45,43 @@ public class GunProjectille : Gun
     {
         base.Update();
         Look();
+       
         ls.transform.position = point_gameObject.transform.position + new Vector3(0, sprite_height1, 0);
+       
+    
 
-      //  Clamp();
-    }
+    //  Clamp();
+}
 
-    new void Look()
+new void Look()
     {
-        ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-
+        Touch f0 = Input.GetTouch(0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
         if (Input.touchCount == 0) return;
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, Ground))
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100000,Ground))
         {
-
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-
-                tobj.transform.position = hit.point;
-                offset = tobj.transform.position - point_gameObject.transform.position;
-
-
-            }
-            if (Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-
-                //tobj.transform.position = hit.point;
-
-                print(offset);
-                point_gameObject.transform.position = hit.point + offset;
-
-
-            }
-            if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
-            {
-                Debug.Log("Ended Touch");
-                offset = Vector3.zero;
-
-            }
-
-            Vector3 diff = point_gameObject.transform.position - transform.position;
-            rot = Quaternion.LookRotation(-diff, Vector3.left);
-            transform.rotation = rot;
+          
+           
+                if (f0.phase == TouchPhase.Began)
+                {
+                    screenPos = Camera.main.WorldToScreenPoint(point_gameObject.transform.position);
+                    offset = point_gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(f0.position.x, f0.position.y, screenPos.z));
+                }
+                else if (f0.phase == TouchPhase.Moved)
+                {
+                    currentScreenPos = new Vector3(f0.position.x, f0.position.y, screenPos.z);
+                    currentPos = Camera.main.ScreenToWorldPoint(currentScreenPos) + offset;
+                    point_gameObject.transform.position = currentPos;
+                }
+            
         }
+    
+
+    Vector3 diff = point_gameObject.transform.position - transform.position;
+         rot = Quaternion.LookRotation(-diff, Vector3.left);
+          transform.rotation = rot;
+        
 
 
 
@@ -115,48 +110,35 @@ public class GunProjectille : Gun
     //new void Look()
     //{
 
+    //        if (Input.GetTouch(0).phase == TouchPhase.Began)
+    //            {
 
-    //    for (var i = 0; i < Input.touchCount; ++i)
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-    //        gettouch = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
-    //        switch (Input.GetTouch(i).phase)
-    //        {
-    //            case TouchPhase.Began:
-    //                if (Physics.Raycast(ray, out hit, Mathf.Infinity, Ground))
-    //                {
+    //                tobj.transform.position = hit.point;
+    //                offset = tobj.transform.position - point_gameObject.transform.position;
 
-    //                    point = hit.point;
-    //                    point.y = 0;
-    //                }
 
-    //                break;
-    //            case TouchPhase.Moved:
+    //            }
+    //            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+    //            {
 
-    //                if (Physics.Raycast(ray, out hit, Mathf.Infinity, Ground))
-    //                {
+    //                //tobj.transform.position = hit.point;
 
-    //                    point = hit.point;
-    //                    point.y = 0;
-    //                    // originalPos = transform.position - hit.point;
+    //                print(offset);
+    //point_gameObject.transform.Translate(tobj.transform.position.x - offset.x,  tobj.transform.position.z - offset.z,0);
 
-    //                    point_gameObject.transform.position += delta * 1 * Time.deltaTime;
 
-    //                    // point_gameObject.transform.Translate(new Vector3(-h, v, 0) * lookSpeed1 * Time.deltaTime);
-    //                    Vector3 diff = point_gameObject.transform.position - transform.position + delta;
-    //                    rot = Quaternion.LookRotation(-diff, Vector3.left);
+    //            }
+    //            if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled)
+    //            {
+    //                Debug.Log("Ended Touch");
+    //                offset = Vector3.zero;
 
-    //                    ls.transform.position = point + new Vector3(0, sprite_height1, 0);
-    //                }
-    //                transform.rotation = rot;
-    //                break;
-    //            case TouchPhase.Ended:
-    //                delta = Vector3.zero;
-    //                point_gameObject.transform.position = Vector3.zero;
-    //                break;
+    //            }
 
+    //            Vector3 diff = point_gameObject.transform.position - transform.position;
+    //rot = Quaternion.LookRotation(-diff, Vector3.left);
+    //            transform.rotation = rot;
     //        }
-
 
 
 
