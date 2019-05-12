3b2d5166dc38ce4a41f76663f916fe4ebbe1f99c
RonakFabain <ronakmanfabiaan@gmail.com>
2019-05-12 22:32:14 +0530

    Cmaera Shake

---
+++ 3D Shooter/Assets/Scripts/CamShake.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+
+    Vector3 startPos;
+    public float shakeTime = 0.5f;
+    public float shakeMagnitude = 1f;
+
+    void Start()
+    {
+
+        InvokeRepeating("CamShake", 0, 0.005f);
+        Invoke("Stop", shakeTime);
+    }
+
+    void Update()
+    {
+        startPos = CamGod.CamPos;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("CamShake", 0, 0.005f);
+            Invoke("Stop", shakeTime);
+        }
+
+    }
+
+    void CamShake()
+    {
+        float deltaX = Random.value * shakeMagnitude;
+        float deltaY = Random.value * shakeMagnitude;
+        Vector3 tempPos = startPos;
+        tempPos.x += deltaX;
+        tempPos.y += deltaY;
+        Camera.main.transform.position = tempPos;
+    }
+
+    void Stop()
+    {
+        CancelInvoke("CamShake");
+        Camera.main.transform.position = startPos;
+
+    }
+}
