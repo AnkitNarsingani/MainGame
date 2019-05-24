0bdb0bbc4714afa6a4881a5b30a7a2fbfa840851
RonakFabain <ronakmanfabiaan@gmail.com>
2019-05-24 19:21:24 +0530

    Heli Move

---
+++ 3D Shooter/Assets/HeliMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliMove : MonoBehaviour
+{
+
+
+    Vector3 pos;
+    Vector3 spos;
+    public float magnitude = 1;
+    public float amplitude = 1;
+    void Start()
+    {
+        pos = transform.position;
+        spos = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float height = 2 * Mathf.Tan(Time.time/amplitude)*magnitude;
+        pos.y = height;
+        transform.position = new Vector3(pos.x, pos.y + spos.y, pos.z);
+
+    }
+}
